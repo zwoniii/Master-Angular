bedc6108e007b9018f6a4ed5b409d4c2a8f8d635
irazv <zwoniii@ya.ru>
2023-05-30 15:27:48 +0300

    34. Combining all Forms of Databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -22,7 +22,7 @@ export class ServersComponent {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
