faee77028e2eb410368f315f911c8c3168fa2300
irazv <zwoniii@ya.ru>
2023-06-05 11:45:23 +0300

    118. Managing Recipes in a Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test','https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg'),
-    new Recipe('Another Recipe', 'This is simply a test','https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg')
-  ];
+  recipes: Recipe[];
+
+  constructor(private RecipeService: RecipeService) {
+
+  }
+
+  ngOnInit(): void {
+    this.recipes = this.RecipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,12 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
-  
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test','https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg'),
+    new Recipe('Another Recipe', 'This is simply a test','https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
