e9e110198fa412ef989d960409c22df52b18f999
irazv <zwoniii@ya.ru>
2023-06-05 12:30:38 +0300

    122. Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,36 @@
 import { EventEmitter } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test','https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg'),
-    new Recipe('Another Recipe', 'This is simply a test','https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg')
+    new Recipe(
+      'Rose punch',
+      'Graceful punch with pink sparkling and berries. Not "drunk" at all, but refreshing and invigorating. Instant heat shot.',
+      'https://www.gastronom.ru/binfiles/images/20170727/b780efa5.jpg',
+      [
+        new Ingredient('Sparkling rose wine', 300),
+        new Ingredient('Watermelon syrup', 50),
+        new Ingredient('Lemon juice', 50),
+        new Ingredient('Sake classic', 50),
+        new Ingredient('Soda', 50)
+      ]
+      ),
+    new Recipe(
+      'Mint gingerbread',
+      'Cocktail Mint gingerbread, also known as gingerbread, as it is prepared with ginger ale and gin-based ginger syrup, will warm you up on a cool spring or summer evening.',
+      'https://www.gastronom.ru/binfiles/images/20190529/b8e0cdfc.jpg',
+      [
+        new Ingredient('Gin', 50),
+        new Ingredient('Syrup "Gingerbread"', 20),
+        new Ingredient('Lemon juice', 20),
+        new Ingredient('Ginger ale', 60),
+        new Ingredient('Mint leaves', 10)
+      ]
+      )
   ];
 
   getRecipes() {
